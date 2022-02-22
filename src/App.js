0c55679f58cca6b3dd6630d33eac514e9d28077b
Prateek <prateekmohanty63@gmail.com>
2022-02-22 13:26:58 +0530

    Feat(Task) added task.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Header from "./components/Header";
+import Task from "./components/Task";
 
 function App() {
   const name='Prateek'
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="container">
      <Header title='Task Tracker' />
+     <Task/>
     </div>
     
   );
