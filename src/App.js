532f9dbe25efef1f71139bc2f74d23c511f9afd7
Prateek <prateekmohanty63@gmail.com>
2022-02-23 23:51:30 +0530

    Implemented fetch api using json server

--- src/App.js
+++ src/App.js
@@ -1,34 +1,34 @@
 import Header from "./components/Header";
 import Task from "./components/Task";
-import {useState} from 'react'
+import {useState,useEffect} from 'react'
 import AddTask from "./components/AddTask";
 
 function App() {
 
   const [showAddTask,setShowAddTask]=useState(false)
-  const [tasks,setTasks]=useState([
+  const [tasks,setTasks]=useState([])
+  
+  // useEffect
 
-    {
-        id:1,
-        text:'Doctors Appointment',
-        day:'Feb 5th at 2:30pm',
-        remainder:false
-    },
-    {
-        id:2,
-        text:'Meeting at School',
-        day:'Feb 6th at 1:30pm',
-        remainder:false
-    },
-    {
-        id:3,
-        text:'Food Shopping',
-        day:'Feb 5th at 2:30pm',
-        remainder:false
-    },
-]
+  useEffect(()=>{
+    const getTasks=async()=>{
+      const tasksFromserver=await fetchTasks()
+      setTasks(tasksFromserver)
+    }
+    getTasks()
+  },[])  // In this array usually dependices are passed
+         /// Such as if user is requried to be registered then we can pass user in this array
+  
 
-  )
+  // Fetch tasks
+  const fetchTasks=async()=>{
+    const res=await fetch('http://localhost:5000/tasks')
+    const data=await res.json()
+    console.log(data)
+
+    return data
+  }
+
   const name='Prateek'
   const x=true
 
