944c04c804cc04679e8c1bde364c0b7b355733c8
Prateek <prateekmohanty63@gmail.com>
2022-02-23 08:05:38 +0530

    Feat(Task) Added useState

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,6 +1,9 @@
 import React from 'react'
+import {useState} from 'react'
 
-const tasks=[
+const Task=()=>{
+  const [tasks,setTasks]=useState([
+
     {
         id:1,
         text:'Doctors Appointment',
@@ -18,17 +21,45 @@ const tasks=[
         text:'Food Shopping',
         day:'Feb 5th at 2:30pm',
         remainder:false
-    }
+    },
 ]
 
-function Task() {
+  )
+
   return (
     <>
-      {tasks.map((task)=>(
+     {tasks.map((task)=>(
        <h3 key={task.id}>{task.text}</h3>
       ))}
     </>
   )
 }
+
+
+
+
+// const tasks=[
+//     {
+//         id:1,
+//         text:'Doctors Appointment',
+//         day:'Feb 5th at 2:30pm',
+//         remainder:false
+//     },
+//     {
+//         id:2,
+//         text:'Meeting at School',
+//         day:'Feb 6th at 1:30pm',
+//         remainder:false
+//     },
+//     {
+//         id:3,
+//         text:'Food Shopping',
+//         day:'Feb 5th at 2:30pm',
+//         remainder:false
+//     }
+// ]
+
+
+
 
 export default Task
